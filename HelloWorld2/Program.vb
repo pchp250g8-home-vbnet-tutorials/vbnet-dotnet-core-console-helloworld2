8fde03ad5294b42a62adda77792a2eb7c1045299
pchp250g8-home <pchp250g8@tutamail.com>
2025-10-01 14:34:02 +0300

    Created Visual Basic .Net Core Cosole Application

---
+++ HelloWorld2/Program.vb
@@ -0,0 +1,10 @@
+Imports System
+
+Module Program
+    Sub Main(args As String())
+        Console.WriteLine("Hello World!")
+        Console.WriteLine("Enter your name:")
+        Dim strName = Console.ReadLine()
+        Console.WriteLine("Hello, " + strName + " !!!")
+    End Sub
+End Module
